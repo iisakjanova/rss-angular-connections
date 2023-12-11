50d2bd2e34403bd5b696557ffe054ef7adff532b
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-12-11 16:15:58 -0600

    feat: implement loading of groups on first page opening

--- connections-app/src/app/components/group-list/group-list.component.ts
+++ connections-app/src/app/components/group-list/group-list.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
@@ -16,28 +16,21 @@ import * as GroupsActions from '../../redux/actions/groups.actions';
   templateUrl: './group-list.component.html',
   styleUrls: ['./group-list.component.scss'],
 })
-export class GroupListComponent {
-  // list = [
-  //   {
-  //     id: '1',
-  //     name: 'group1',
-  //     createdAt: '01/01/2024',
-  //     createdBy: 'User',
-  //   },
-  //   {
-  //     id: '2',
-  //     name: 'group2',
-  //     createdAt: '01/01/2024',
-  //     createdBy: 'User',
-  //   },
-  // ];
-
+export class GroupListComponent implements OnInit {
   $list = this.store.select(selectGroups);
 
   constructor(
     private store: Store,
     private authService: AuthService
   ) {}
+
+  ngOnInit(): void {
+    this.$list.subscribe(list => {
+      if (list.length === 0) {
+        this.update();
+      }
+    });
+  }
 
   update() {
     const credentials = this.authService.getCredentials();
