95e1cde921ac24cf4e7511fd05166c84877b5b10
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-12-15 20:15:18 -0600

    feat: add basic layout for people component

---
+++ connections-app/src/app/components/people-list/people-list.component.ts
@@ -0,0 +1,96 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import {
+  selectGroups,
+  selectGroupsError,
+  selectGroupsLoading,
+} from 'src/app/redux/selectors/groups.selectors';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { CountdownService } from 'src/app/services/countdown/countdown.service';
+
+import * as GroupsActions from '../../redux/actions/groups.actions';
+
+@Component({
+  selector: 'app-people-list',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatListModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  templateUrl: './people-list.component.html',
+  styleUrls: ['./people-list.component.scss'],
+})
+export class PeopleListComponent implements OnInit {
+  list$ = this.store.select(selectGroups);
+
+  countdown$ = this.countdownService.countdown$;
+
+  error$ = this.store.select(selectGroupsError);
+
+  loading$ = this.store.select(selectGroupsLoading);
+
+  private errorSubscription: Subscription | undefined;
+
+  private credentials;
+
+  constructor(
+    private store: Store,
+    private authService: AuthService,
+    private countdownService: CountdownService
+  ) {
+    this.credentials = this.authService.getCredentials();
+  }
+
+  ngOnInit(): void {
+    this.list$.subscribe(list => {
+      if (list.length === 0) {
+        this.getGroups();
+      }
+    });
+  }
+
+  getGroups() {
+    this.store.dispatch(GroupsActions.getGroups(this.credentials));
+  }
+
+  update() {
+    if (this.errorSubscription) {
+      this.errorSubscription.unsubscribe();
+      this.errorSubscription = undefined;
+    }
+
+    // Subscribe to the new error$ observable
+    this.errorSubscription = this.error$.subscribe(error => {
+      if (!error) {
+        this.countdownService.startCountdown();
+      } else {
+        this.countdownService.stopCountdown();
+        this.countdownService.resetCountdown();
+      }
+    });
+
+    this.getGroups();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  isButtonDisabled(
+    loading: boolean | null,
+    countdownValue: number | null
+  ): boolean {
+    // If loading is null, treat it as false
+    const isLoading = loading !== null ? loading : false;
+    // If countdownValue is null, treat it as 0
+    const countdown = countdownValue !== null ? countdownValue : 0;
+
+    return isLoading || (countdown < 60 && countdown !== 0);
+  }
+}
--- connections-app/src/app/pages/main-page/main-page.component.ts
+++ connections-app/src/app/pages/main-page/main-page.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { GroupListComponent } from 'src/app/components/group-list/group-list.component';
+import { PeopleListComponent } from 'src/app/components/people-list/people-list.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [CommonModule, GroupListComponent],
+  imports: [CommonModule, GroupListComponent, PeopleListComponent],
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
 })
